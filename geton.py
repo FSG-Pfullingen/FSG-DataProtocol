d23690e0da7e3bd7168cd33d1e528c07ca1e4445
GQDeltex <operitum@gmail.com>
2017-06-29 21:53:50 +0200

    Changed it a bit and added more functionality!

--- geton.py
+++ geton.py
@@ -1,10 +1,11 @@
 import FSGDP
 import multiprocessing as mp
-import logging
 import time
 
-def get_data(data_in, clock_in, data_out, clock_out):
-    Recv = FSGDP.Receiver(data_out, clock_out)
+# Multiprocessing: https://docs.python.org/2/library/multiprocessing.html#exchanging-objects-between-processes
+
+def get_data((data_out, clock_out, data_in, clock_in), queue):
+    Recv = FSGDP.Receiver(data_in, clock_in)
     print ("Waiting for response")
     meta, data = Recv.receive()
     print (meta)
@@ -12,31 +13,37 @@ def get_data(data_in, clock_in, data_out, clock_out):
     from_adress = int(meta[1])
     print (str(to_adress), str(from_adress))
     data = Recv.make_hr(data)
-    Sender = FSGDP.Sender(data_in, clock_in)
+    queue.put((to_adress, from_adress, data))
+    Sender = FSGDP.Sender(data_out, clock_out)
     Sender.adress = from_adress
     Sender.send(data, to_adress)
     FSGDP.close_connection()
 
-def main():
-    process_list = []
-    mp.log_to_stderr()
-    logger = mp.get_logger()
-    logger.setLevel(logging.INFO)
+def open_interface(pin_list, q):
     print("Processors: " + str(mp.cpu_count()))
-    p1 = mp.Process(target=get_data, args=(13, 15, 16, 18,))
-    #p2 = mp.Process(target=get_data, args=(19, 21, 22, 24,))
-    process_list.append(p1)
-    #process_list.append(p2)
-    print(process_list)
-    for process in process_list:
+    process_list = []
+    for pins in pin_list:
+        print (pins)
+        process = mp.Process(target=get_data, args=(pins, q,))
+        process_list.append(process)
         process.start()
+    return process_list
+
+def renew_process(input_list):
+    for process in input_list:
+        if not process.is_alive():
+            process.terminate()
+            time.sleep(1)
+            process.start()
+            print("New Process:" + str(process.name))
+
+def main():
+    Pinout = [(13, 15, 16, 18), (19, 21, 22, 24)] # (data_out, clock_out, data_in, clock_in)
+    process_list = open_interface(Pinout)
+    q = mp.Queue()
     while True:
-        for process in process_list:
-            if not process.is_alive():
-                process.terminate()
-                time.sleep(0.5)
-                process.start()
-                print("New Process:" + str(process.name))
+        renew_process(process_list, q)
+        print("In Queue: ", str(q.get()))
         time.sleep(0.5)
 
 if __name__ == "__main__":
